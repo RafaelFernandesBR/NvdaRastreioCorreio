3476f70190bb2d58db07e2c3d6aecc2c7f843e71
Rafael Fernandes <marcorafaeldemelo16@gmail.com>
2023-08-20 20:20:15 -0300

    corrigindo erros de código e melhorias ao exibir todos os státus

--- addon/globalPlugins/rastreios.py
+++ addon/globalPlugins/rastreios.py
@@ -1,6 +1,24 @@
+import globalPluginHandler
+import ui
+import api
 import json
 from urllib.request import urlopen, Request
 import ui
+
+
+def get_rastreio(code):
+    url = f"http://androidparacegos.com.br/rastrear/{code}"
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 tSafari/537.36'}
+    request = Request(url, headers=headers)
+    response = urlopen(request)
+    data = json.loads(response.read().decode('utf-8'))
+
+    if not data:
+        raise ValueError("Não foi possível obter os dados da API")
+
+    return data
+
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
     def script_obter_ultimo_evento(self, gesture):
@@ -18,24 +36,15 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
     def script_todos_eventos(self, gesture):
         clipboard_text = api.getClipData()
         send = get_rastreio(clipboard_text)
+        eventos = ""
+
         for evento in send['eventos']:
             dt_hr_criado = f"{evento['data']} {evento['hora']}"
-            msg_text = f"{evento['status']}, em {evento['local']}. Data: {dt_hr_criado}"
-            ui.browseableMessage(msg_text, title="Eventos rastreio")
+            eventos += f"{evento['status']}, em {evento['local']}. Data: {dt_hr_criado}\n"
+
+        ui.browseableMessage(eventos, title="Eventos rastreio")
 
     __gestures = {
         "kb:nvda+e": "obter_ultimo_evento",
         "kb:nvda+control+e": "todos_eventos"
     }
-
-def get_rastreio(code):
-    url = f"http://androidparacegos.com.br/rastrear/{code}"
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 tSafari/537.36'}
-    request = Request(url, headers=headers)
-    response = urlopen(request)
-    data = json.loads(response.read().decode('utf-8'))
-    
-    if not data:
-        raise ValueError("Não foi possível obter os dados da API")
-    
-    return data
