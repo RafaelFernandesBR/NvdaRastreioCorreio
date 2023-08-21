b95a1d4529c6df3ae2dd8811fc6a1aa2aed59807
Rafael Fernandes <marcorafaeldemelo16@gmail.com>
2023-08-21 12:31:33 -0300

    mudança na forma de criar script no addon, e adicionada documentação do addon.

--- addon/globalPlugins/rastreios.py
+++ addon/globalPlugins/rastreios.py
@@ -1,4 +1,5 @@
 import globalPluginHandler
+from scriptHandler import script
 import ui
 import api
 import json
@@ -20,6 +21,12 @@ def get_rastreio(code):
 
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+    scriptCategory = "RastreioCorreios"
+
+    @script(
+        description="Obtém o último evento de um código de rastreio.",
+        gestures=["kb:nvda+e"]
+    )
     def script_obter_ultimo_evento(self, gesture):
         clipboard_text = api.getClipData()
         send = get_rastreio(clipboard_text)
@@ -29,21 +36,22 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
             return
 
         dt_hr_criado = f"{evento['data']} {evento['hora']}"
-        msg_text = f"{evento['status']}, em {evento['local']}. Data: {dt_hr_criado}"
+        substatus_text = ", ".join(evento['subStatus'])
+        msg_text = f"{evento['status']}, em {evento['local']}, {substatus_text}. Data: {dt_hr_criado}"
         ui.message(msg_text)
 
+    @script(
+        description="Obtém todos os eventos de um código de rastreio.",
+        gestures=["kb:nvda+control+e"]
+    )
     def script_todos_eventos(self, gesture):
         clipboard_text = api.getClipData()
         send = get_rastreio(clipboard_text)
-        eventos = ""
+        eventos = f"Código: {clipboard_text}\n"
 
         for evento in send['eventos']:
             dt_hr_criado = f"{evento['data']} {evento['hora']}"
-            eventos += f"{evento['status']}, em {evento['local']}. Data: {dt_hr_criado}\n"
+            substatus_text = ", ".join(evento['subStatus'])
+            eventos += f"{evento['status']}, em {evento['local']}, {substatus_text}. Data: {dt_hr_criado}\n"
 
         ui.browseableMessage(eventos, title="Eventos rastreio")
-
-    __gestures = {
-        "kb:nvda+e": "obter_ultimo_evento",
-        "kb:nvda+control+e": "todos_eventos"
-    }
