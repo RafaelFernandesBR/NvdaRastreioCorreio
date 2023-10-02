5238365ed0c3138abbc835e0684dbb509a9d2cdd
Rafael Fernandes <marcorafaeldemelo16@gmail.com>
2023-10-02 14:22:00 -0300

    tratamento de erro ao mandar código inválido na api

--- addon/globalPlugins/rastreios/__init__.py
+++ addon/globalPlugins/rastreios/__init__.py
@@ -10,85 +10,94 @@ import gui
 
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
-	scriptCategory = "RastreioCorreios"
+    scriptCategory = "RastreioCorreios"
 
-	@script(
-		# Gets the last event of a trace code.
-		description="Obtém o último evento de um código de rastreio.",
-		gestures=["kb:nvda+e"]
-	)
-	def script_Gets_The_Last_Event(self, gesture):
-		request = RequestCorreios.RequestCorreios()
-		clipboardCode = api.getClipData()
-		tracking = request.get_tracking(clipboardCode)
+    @script(
+        # Gets the last event of a trace code.
+        description="Obtém o último evento de um código de rastreio.",
+        gestures=["kb:nvda+e"]
+    )
+    def script_Gets_The_Last_Event(self, gesture):
+        request = RequestCorreios.RequestCorreios()
+        clipboardCode = api.getClipData()
+        try:
+            tracking = request.get_tracking(clipboardCode)
 
-		if tracking['quantidade'] == 0:
-			# If events are not returned in the api.
-			ui.message(
-				"Não foi possível obter os dados do código informado. Se a remeça foi enviada recentimente, aguarde até 24 horas e tente novamente!")
-			return
+            if tracking['quantidade'] == 0:
+                # If events are not returned in the api.
+                ui.message(
+                    "Não foi possível obter os dados do código informado. Se a remeça foi enviada recentimente, aguarde até 24 horas e tente novamente!")
+                return
 
-		evento = tracking['eventos'][0]
+            evento = tracking['eventos'][0]
 
-		Date_And_Time = f"{evento['data']} {evento['hora']}"
-		substatus_text = ", ".join(evento['subStatus'])
-		Complete_Text = f"{evento['status']}, em {evento['local']}, {substatus_text}. Data: {Date_And_Time}"
-		ui.message(Complete_Text)
+            Date_And_Time = f"{evento['data']} {evento['hora']}"
+            subsubstatus_text = ", ".join(evento['subStatus'])
+            Complete_Text = f"{evento['status']}, em {evento['local']}, {substatus_text}. Data: {Date_And_Time}"
+            ui.message(Complete_Text)
+        except Exception as e:
+            ui.message(str(e))
 
-	@script(
-		# Gets all events for a trace code.
-		description="Obtém todos os eventos de um código de rastreio.",
-		gestures=["kb:nvda+control+e"]
-	)
-	def script_Gets_The_All_Event(self, gesture):
-		request = RequestCorreios.RequestCorreios()
-		clipboardCode = api.getClipData()
-		tracking = request.get_tracking(clipboardCode)
+    @script(
+        # Gets all events for a trace code.
+        description="Obtém todos os eventos de um código de rastreio.",
+        gestures=["kb:nvda+control+e"]
+    )
+    def script_Gets_The_All_Event(self, gesture):
+        request = RequestCorreios.RequestCorreios()
+        clipboardCode = api.getClipData()
+        try:
+            tracking = request.get_tracking(clipboardCode)
 
-		if tracking['quantidade'] == 0:
-			# If events are not returned in the api.
-			ui.message(
-				"Não foi possível obter os dados do código informado. Se a remeça foi enviada recentimente, aguarde até 24 horas e tente novamente!")
-			return
+            if tracking['quantidade'] == 0:
+                # If events are not returned in the api.
+                ui.message(
+                    "Não foi possível obter os dados do código informado. Se a remeça foi enviada recentimente, aguarde até 24 horas e tente novamente!")
+                return
 
-		# loop through the events, and add them all to a string.
-		for event in tracking['eventos']:
-			Date_And_Time = f"{event['data']} {event['hora']}"
-			substatus_text = ", ".join(event['subStatus'])
-			events += f"{event['status']}, em {event['local']}, {substatus_text}. Data: {Date_And_Time}\n"
+            # loop through the events, and add them all to a string.
+            for event in tracking['eventos']:
+                Date_And_Time = f"{event['data']} {event['hora']}"
+                substatus_text = ", ".join(event['subStatus'])
+                events += f"{event['status']}, em {event['local']}, {substatus_text}. Data: {Date_And_Time}\n"
 
-		ui.browseableMessage(events, title=clipboardCode)
+                ui.browseableMessage(events, title=clipboardCode)
+        except Exception as e:
+            ui.message(str(e))
 
-	@script(
-		# Gets events for a user-entered trace code.
-		description="Obtém eventos para um código de rastreio digitado pelo usuário.",
-		gestures=["kb:nvda+shift+control+e"]
-	)
-	def script_Gets_Events_By_User_Code(self, gesture):
-		dlg = wx.TextEntryDialog(
-			gui.mainFrame, "Digite o código de rastreio.", "código de rastreio")
-		code = ""
+    @script(
+        # Gets events for a user-entered trace code.
+        description="Obtém eventos para um código de rastreio digitado pelo usuário.",
+        gestures=["kb:nvda+shift+control+e"]
+    )
+    def script_Gets_Events_By_User_Code(self, gesture):
+        dlg = wx.TextEntryDialog(
+            gui.mainFrame, "Digite o código de rastreio.", "código de rastreio")
+        code = ""
 
-		def callback(result):
-			if result == wx.ID_OK:
-				code = dlg.GetValue()
-				request = RequestCorreios.RequestCorreios()
-				tracking = request.get_tracking(code)
+        def callback(result):
+            if result == wx.ID_OK:
+                code = dlg.GetValue()
+                request = RequestCorreios.RequestCorreios()
+                try:
+                    tracking = request.get_tracking(code)
 
-				if tracking['quantidade'] == 0:
-					# If events are not returned in the api.
-					ui.message(
-						"Não foi possível obter os dados do código informado. Se a remeça foi enviada recentimente, aguarde até 24 horas e tente novamente!")
-					return
+                    if tracking['quantidade'] == 0:
+                        # If events are not returned in the api.
+                        ui.message(
+                            "Não foi possível obter os dados do código informado. Se a remeça foi enviada recentimente, aguarde até 24 horas e tente novamente!")
+                        return
 
-				events = f"Código: {code}\n"
+                    events = f"Código: {code}\n"
 
-		# loop through the events, and add them all to a string.
-				for event in tracking['eventos']:
-					Date_And_Time = f"{event['data']} {event['hora']}"
-					substatus_text = ", ".join(event['subStatus'])
-					events += f"{event['status']}, em {event['local']}, {substatus_text}. Data: {Date_And_Time}\n"
+            # loop through the events, and add them all to a string.
+                    for event in tracking['eventos']:
+                        Date_And_Time = f"{event['data']} {event['hora']}"
+                        substatus_text = ", ".join(event['subStatus'])
+                        events += f"{event['status']}, em {event['local']}, {substatus_text}. Data: {Date_And_Time}\n"
 
-				ui.browseableMessage(events, title="Eventos rastreio")
+                    ui.browseableMessage(events, title="Eventos rastreio")
+                except Exception as e:
+                    ui.message(str(e))
 
-		gui.runScriptModalDialog(dlg, callback)
+        gui.runScriptModalDialog(dlg, callback)
--- addon/globalPlugins/rastreios/requestCorreios.py
+++ addon/globalPlugins/rastreios/requestCorreios.py
@@ -1,5 +1,6 @@
 import json
 from urllib.request import urlopen, Request
+from urllib.error import HTTPError  # Importe a classe HTTPError
 
 
 class RequestCorreios:
@@ -7,12 +8,16 @@ class RequestCorreios:
 	def get_tracking(self, code):
 		url = f"http://androidparacegos.com.br/rastrear/{code}"
 		headers = {
-			'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 tSafari/537.36'}
+			'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+		}
 		request = Request(url, headers=headers)
-		response = urlopen(request)
-		events = json.loads(response.read().decode('utf-8'))
 
-		if not events:
-			raise ValueError("Não foi possível obter os dados da API")
+		try:
+			response = urlopen(request)
+			events = json.loads(response.read().decode('utf-8'))
 
-		return events
+			return events
+		except HTTPError as e:
+			if e.code == 400:
+				error_message = json.loads(e.read().decode('utf-8'))
+				raise ValueError(error_message['mensagem'])
